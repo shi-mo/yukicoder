09479426bd1321cca6399a715743587025306ae7
shi-mo <yoshifumi.shimono@gmail.com>
2016-03-28 13:56:04 +0900

    Refactor d0003.d

--- d0003.d
+++ d0003.d
@@ -3,30 +3,28 @@ import core.bitop;
 
 int[] numMoves;
 
-void moveFrom(uint i, uint n) {
-   if (!numMoves[i-1]) { return; }
+void main() {
+    uint n;
+    readf("%s\n", &n);
 
-   foreach(neighbor; [ (i + i.popcnt), (i - i.popcnt) ]) {
-       if (neighbor < 1 || n < neighbor) { continue; }
-
-       int nmNeighbor = numMoves[i-1] + 1;
-       if (numMoves[neighbor-1] && (numMoves[neighbor-1] <= nmNeighbor)) {
-           continue;
-       }
-
-       numMoves[neighbor-1] = nmNeighbor;
-       moveFrom(neighbor, n);
-   }
+    numMoves.length = n;
+    numMoves[0] = 1;
+    foreach (i; 1..n) {
+        moveFrom(i, n);
+    }
+    writeln(numMoves[n-1] ? numMoves[n-1] : -1);
 }
 
-void main() {
-   uint n;
-   readf("%s\n", &n);
+void moveFrom(uint i, uint n) {
+    if (!numMoves[i-1]) { return; }
 
-   numMoves.length = n;
-   numMoves[0] = 1;
-   foreach (i; 1..n) {
-       moveFrom(i, n);
-   }
-   writeln(numMoves[n-1] ? numMoves[n-1] : -1);
+    foreach(k; [ (i + i.popcnt), (i - i.popcnt) ]) {
+        if (k < 1 || n < k) { continue; }
+
+        int nm = numMoves[i-1] + 1;
+        if (numMoves[k-1] && (numMoves[k-1] <= nm)) { continue; }
+
+        numMoves[k-1] = nm;
+        moveFrom(k, n);
+    }
 }
