32ca432635b50e850ecca40ac55811a95f59d4e3
shi-mo <yoshifumi.shimono@gmail.com>
2016-03-28 14:06:56 +0900

    Refactor 0045.rb

--- 0045.rb
+++ 0045.rb
@@ -1,12 +1,9 @@
 n = gets.to_i
 v = [nil] + gets.chomp!.split.map(&:to_i)
 
-dp  = [ 0, v[1] ] # scores include i-th dish
-max = [ 0, v[1] ] # max value of dp[0..i]
-
+dp  = [ 0, v[1] ]
 2.upto(n) do |i|
-  dp[i] = v[i] + max[i-2]
-  max[i] = [ dp[i], max[i-1] ].max
+  dp[i] = [ v[i] + dp[i-2], dp[i-1] ].max
 end
 
-puts [ dp[n], dp[n-1] ].max
+puts dp[n]
