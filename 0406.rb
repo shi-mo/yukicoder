bdad9f134606b3841710b876113caaa0847c4faa
shi-mo <yoshifumi.shimono@gmail.com>
2016-08-31 21:50:23 +0900

    Improve performance: No.406 [Ruby].

--- 0406.rb
+++ 0406.rb
@@ -2,9 +2,13 @@ n = gets.to_i
 x = gets.split.map(&:to_i).sort
 
 d = x[1] - x[0]
-n.times do |i|
-  next if i < 2
-  if x[i] == x[i-1] || (x[i] - x[i-1]) != d
+if 0 == d
+  puts 'NO'
+  exit 0
+end
+
+2.upto(n-1) do |i|
+  if (x[i] - x[i-1]) != d
     puts 'NO'
     exit 0
   end
