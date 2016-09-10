2977605a6d72f7e81920c4fd4a7378ad14861f9f
shi-mo <yoshifumi.shimono@gmail.com>
2016-09-10 16:22:48 +0900

    Improve algorithm: 0273.rb

--- 0273.rb
+++ 0273.rb
@@ -1,41 +1,22 @@
 s = gets.chomp
 
 n = s.length
-$dp_valid  = Array.new(n+1){ Array.new(n+1) }
-$dp_answer = Array.new(n+1){ Array.new(n+1) }
 
 def is_valid?(s, i, j)
-  return $dp_valid[i][j] unless $dp_valid[i][j].nil?
-
   len = i - j
-  return ($dp_valid[i][j] = true) if 1 == len
+  return true if 1 == len
 
   center = (i + j) / 2
   if 0 == (len % 2)
-    return $dp_valid[i][j] = (s[i..(center-1)] == s[center..(j-1)].reverse)
+    return s[i..(center-1)] == s[center..(j-1)].reverse
   end
-  return $dp_valid[i][j] = (s[i..(center-1)] == s[(center+1)..(j-1)].reverse)
+  s[i..(center-1)] == s[(center+1)..(j-1)].reverse
 end
 
-def solve(s, i, j)
-  return $dp_answer[i][j] if $dp_answer[i][j]
-
-  if i == j
-    return $dp_answer[i][j] = 0
+ans = [1]
+0.upto(n-1) do |i|
+  i.upto(n) do |j|
+    ans << (j-i) if is_valid?(s, i, j)
   end
-  if is_valid?(s, i, j)
-    return $dp_answer[i][j] = j - i
-  end
-
-  ans = 0
-  (i+1).upto(j-1) do |k|
-    ans = [ans, [solve(s, i, k), solve(s, k, j)].max].max
-  end
-  return $dp_answer[i][j] = ans
 end
-
-ans = 0
-1.upto(n-1) do |k|
-  ans = [ans, [solve(s, 0, k), solve(s, k, n)].max].max
-end
-puts ans
+puts (ans - [n]).max
