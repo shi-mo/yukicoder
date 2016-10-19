481fe44123196ed2f04784ef07d1f53b8208b99b
shi-mo <yoshifumi.shimono@gmail.com>
2016-10-20 00:42:04 +0900

    Improve algorithm: No.11 [Ruby].

--- 0011.rb
+++ 0011.rb
@@ -1,18 +1,7 @@
 w, h, n = 3.times.map{ gets.to_i }
 sk = n.times.map{ gets.split.map(&:to_i) }
 
-s2k = sk.group_by{|s, k| s }
-k2s = sk.group_by{|s, k| k }
-s = s2k.keys
+s = sk.map{|s, k| s }.uniq
+k = sk.map{|s, k| k }.uniq
 
-num_match = 0
-s2k.each do |s, elems|
-  num_match += h - elems.size
-end
-
-k2s.each do |k, elems|
-  num_match += w - elems.size
-  num_match -= (s - elems.map{|si, _| si }).size
-end
-
-puts num_match
+puts (w * h) - ((w - s.size) * (h - k.size)) - n
