a591dfbbc7cdc3aacce6b07c5891980950222c80
shi-mo <yoshifumi.shimono@gmail.com>
2016-10-20 20:38:02 +0900

    Improve algorithm: No.414 [Ruby].

--- 0414.rb
+++ 0414.rb
@@ -1,13 +1,8 @@
 m = gets.to_i
 
 def get_divisor(n)
-  i = 2
-  x = n / 2
-  while i <= x
+  2.upto(Math.sqrt(n)) do |i|
     return i if 0 == (n % i)
-
-    i += 1
-    x = n / i
   end
   nil
 end
