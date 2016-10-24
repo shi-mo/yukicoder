afac9dde2fe2c279930441676fd7f8fd92115825
shi-mo <yoshifumi.shimono@gmail.com>
2016-10-24 19:28:00 +0900

    Answer for No.171 [Ruby].

---
+++ 0171.rb
@@ -0,0 +1,12 @@
+s = gets.chomp
+
+def factorial(i)
+  (1..i).to_a.inject(:*)
+end
+
+c = s.split(//o).group_by{|a| a }.map{|_, ary| ary.size }
+n = factorial(s.length)
+c.each do |m|
+  n /= factorial(m)
+end
+puts (n - 1) % 573
