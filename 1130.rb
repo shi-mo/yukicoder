6bff78eaae32dc929384a87048ad6b66594cefad
shi-mo <yoshifumi.shimono@gmail.com>
2020-09-29 16:29:12 +0900

    Simplify code: No.1130 [Ruby].

--- 1130.rb
+++ 1130.rb
@@ -1,12 +1,9 @@
 h, w = gets.split.map(&:to_i)
 a = []
-h.times{ a << gets.split.map(&:to_i) }
+h.times{ a.concat gets.split.map(&:to_i) }
 
-b = a.flatten.sort
-a = []
+a.sort!
 h.times do |i|
   s = i * w
-  a << b[s..(s+w-1)]
+  puts a[s..(s+w-1)].join(' ')
 end
-
-h.times{|i| puts a[i].join(' ') }
