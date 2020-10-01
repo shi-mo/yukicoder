90ccbfb73346a8d7612b889cb8d18a7f6a6f9c4c
shi-mo <yoshifumi.shimono@gmail.com>
2020-10-01 23:31:09 +0900

    Shorten code: No.52 [Ruby].

--- 0052.rb
+++ 0052.rb
@@ -1,8 +1,5 @@
-s = gets.chomp!
-
 def solve(a, s)
-  return [a + s] if s.length <= 1
-
-  solve(a + s[-1], s[0..-2]) + solve(a + s[0], s[1..-1])
+  return [a+s] if s.length <= 1
+  solve(a+s[-1], s[0..-2]) + solve(a+s[0], s[1..])
 end
-puts solve("", s).uniq.size
+puts solve('', gets.chomp).uniq.size
